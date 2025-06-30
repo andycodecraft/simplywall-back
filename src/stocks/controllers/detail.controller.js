5892b0295400b2bdc0d421f2dc3964bfed2760eb
andycodecraft <andrewhome1125@gmail.com>
2025-06-30 09:37:53 -0700

    add multiple random chart import

--- src/stocks/controllers/detail.controller.js
+++ src/stocks/controllers/detail.controller.js
@@ -41,39 +41,52 @@ const getSignedUrlAsync = (params) => {
   });
 };
 
+function extractContent(text) {
+  const imageLinkRegex = /\[?(Images\/[^\]\s]+)\]?/g;
+
+  const imageLinks = [];
+  let match;
+  while ((match = imageLinkRegex.exec(text)) !== null) {
+    imageLinks.push(match[1]);
+  }
+  return imageLinks;
+}
+
 exports.getTopStockById = async (req, res) => {
   try {
     const encrypt_id = decodeURIComponent(req.body.stock_id);
     const secretKey = 'mySecretKey';
     const bytes = CryptoJS.AES.decrypt(encrypt_id, secretKey);
     const stock_id = bytes.toString(CryptoJS.enc.Utf8);
+
     const query = `SELECT * FROM top_ideas WHERE id = ${stock_id}`;
     const [details] = await pool.query(query);
 
     const content_query = `SELECT * FROM idea_details WHERE idea_id = ${stock_id}`;
     let [contents] = await pool.query(content_query);
-
+    // console.log(contents)
     if (contents) {
       const tasks = contents.map(async (content) => {
-        if (content['chart_image']) {
-          const params = {
-            Bucket: 'superid-llm-test',
-            Key: content['chart_image'],
-          };
-
-          try {
-            const url = await getSignedUrlAsync(params);
-            content['chart_image'] = url;
-          } catch (error) {
-            console.error('Error getting signed URL:', error);
-          }
+        if (content['content']) {
+            const imageLinks = extractContent(content['content'])
+            for (const imageLink of imageLinks) {
+              const params = {
+                Bucket: 'superid-llm-test',
+                Key: imageLink,
+              };
+      
+              try {
+                const url = await getSignedUrlAsync(params);
+                content['content'] = content['content'].split('[' + imageLink + ']').join('[' + url + ']');
+              } catch (error) {
+                console.error('Error getting signed URL:', error);
+              }
+            }
         }
         return content;
       });
-
       contents = await Promise.all(tasks);
     }
-
     if (details) {
       details[0]['detail'] = contents;
     }
