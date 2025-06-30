b4663f366f4a36cf84e616bf61cf30c38d43b033
andycodecraft <andrewhome1125@gmail.com>
2025-06-30 00:35:05 -0700

    add: stock chart image link

--- src/stocks/controllers/detail.controller.js
+++ src/stocks/controllers/detail.controller.js
@@ -1,5 +1,13 @@
 const CryptoJS = require('crypto-js');
 const pool = require('../connection');
+const AWS = require('aws-sdk');
+const path = require('path');
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'us-east-1'
+});
 
 exports.getTopStocks = async (req, res) => {
   try {
@@ -21,35 +29,66 @@ exports.getTopStocks = async (req, res) => {
   }
 }
 
+const getSignedUrlAsync = (params) => {
+  return new Promise((resolve, reject) => {
+    s3.getSignedUrl('getObject', params, (err, url) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(url);
+      }
+    });
+  });
+};
+
 exports.getTopStockById = async (req, res) => {
   try {
     const encrypt_id = decodeURIComponent(req.body.stock_id);
-
     const secretKey = 'mySecretKey';
     const bytes = CryptoJS.AES.decrypt(encrypt_id, secretKey);
     const stock_id = bytes.toString(CryptoJS.enc.Utf8);
-    
     const query = `SELECT * FROM top_ideas WHERE id = ${stock_id}`;
     const [details] = await pool.query(query);
 
     const content_query = `SELECT * FROM idea_details WHERE idea_id = ${stock_id}`;
-    const [contents] = await pool.query(content_query);
-    
+    let [contents] = await pool.query(content_query);
+
+    if (contents) {
+      const tasks = contents.map(async (content) => {
+        if (content['chart_image']) {
+          const params = {
+            Bucket: 'superid-llm-test',
+            Key: content['chart_image'],
+          };
+
+          try {
+            const url = await getSignedUrlAsync(params);
+            content['chart_image'] = url;
+          } catch (error) {
+            console.error('Error getting signed URL:', error);
+          }
+        }
+        return content;
+      });
+
+      contents = await Promise.all(tasks);
+    }
+
     if (details) {
-      details[0]['detail'] = contents
+      details[0]['detail'] = contents;
     }
 
     const resData = {
       status: true,
-      response: details
-    }
+      response: details,
+    };
 
     res.status(200).json(resData);
   } catch (error) {
     const resData = {
       status: false,
-      response: error.message
-    }
+      response: error.message,
+    };
     res.status(400).json(resData);
   }
-}
+};
